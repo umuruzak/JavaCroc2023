bddd9cc4d99152610c4982ab013e0a994e348d9d
Arkady Umurzakov <53270320+umuruzak@users.noreply.github.com>
2023-10-09 17:32:36 +0300

    Add files via upload

---
+++ Задание №1/Task1.java
@@ -0,0 +1,11 @@
+/**
+*   Task №1
+*   Umurzakov A., 09.10.23
+*   Prints 'Hello, Java ^^'
+*/
+public class Task1{
+
+    public static void main(String[] args){
+        System.out.println("Hello, Java ^^");
+    }
+}
