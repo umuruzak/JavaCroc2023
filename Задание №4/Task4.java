6f1586956512ef42d44ccbce1dd9f698fbf17f89
Arkady Umurzakov <53270320+umuruzak@users.noreply.github.com>
2023-10-18 12:18:42 +0300

    Add files via upload

---
+++ Задание №4/Task4.java
@@ -0,0 +1,26 @@
+/**
+*   Task №3
+*   Umurzakov A., 18.10.23
+*/
+
+public class Task4{
+    
+    public static void main(String[] args){
+        try{
+            int first = Integer.parseInt(args[0]);
+            int diff = Integer.parseInt(args[1]);
+            int number = Integer.parseInt(args[2]);
+            int nTh = first;
+            int sum = 0;
+            
+            for (int i = 1; i < number; i++){
+                sum += nTh;
+                nTh += diff;
+            }
+            sum += nTh;
+            System.out.println("Sum: " + sum);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
